812485178c72c01e1bf5a38a8c535d39324664af
samstevens127 <samuelstevens03@gmail.com>
2025-04-29 10:22:47 -0400

    nvim: changed colourscheme

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -33,7 +33,7 @@ vim.g.vimtex_view_general_viewer = 'mupdf'
 vim.g.vimtex_view_method = "mupdf"
 
 -- colorcheme settings
-vim.cmd.colorscheme "base16-chalk"
+vim.cmd.colorscheme "base16-dracula" -- base16-cupertino for light theme
 
 -- autoclose setup
 require("autoclose").setup()
